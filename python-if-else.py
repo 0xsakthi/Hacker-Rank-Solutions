5650ca1dd5553c71ca76eca402be54d297001d50
sakthigrammer <82046832+sakthigrammer@users.noreply.github.com>
2021-06-29 06:55:20 +0530

    Add files via upload

---
+++ python-if-else.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n%2==1:
+  print('Weird')
+elif n%2==0 and 2 <= n <=5:
+	print('Not Weird')
+elif n%2==0 and 6 <= n <=20:
+	print('Weird')
+elif n%2==0 and n > 20:
+	print('Not Weird')
+
+
