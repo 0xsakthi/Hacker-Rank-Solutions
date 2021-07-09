d4be724c1d91103adb778d3df4c3370f670f0b1f
sakthigrammer <82046832+sakthigrammer@users.noreply.github.com>
2021-07-09 19:33:59 +0530

    Update (11)Finding-the-percentage.py

--- (11)Finding-the-percentage.py
+++ (11)Finding-the-percentage.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
         name, *line = input().split() #spliting and print/add a value in one line so use *list
         scores = list(map(float, line)) #convert to float
         student_marks[name] = scores #convert to dictionary
-    query_name = input() #to get marks to given name 
+    query_name = input() #to get marks to given format(name 
     marks = student_marks[query_name] #store a student marks
-    print(sum(marks)/n) #sum and divided by n(3) 
+    print(format(sum(marks)/3,'.2f')) #sum and divided by 3 
